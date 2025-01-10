44f0699fa2a7c5683c1d4a034f59e1d0b246f21d
super-kimchi <powercoding1004@gmail.com>
2025-01-10 03:14:23 +0100

    memcopy, extractline, leftover

--- get_next_line.c
+++ get_next_line.c
@@ -6,16 +6,19 @@
 /*   By: kyungkim <kyungkim@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/07 22:46:37 by kyungkim          #+#    #+#             */
-/*   Updated: 2025/01/07 23:14:14 by kyungkim         ###   ########.fr       */
+/*   Updated: 2025/01/10 00:38:56 by kyungkim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+
+
 char *get_next_line(int fd)
 {
 	static char *buffer;
 	char	*line;
+	int		new_i;
 
 	if (!buffer)
 	{
@@ -24,18 +27,15 @@ char *get_next_line(int fd)
 			return (0);
 		*buffer = '\0';
 	}
-	else if
+	else
 	{
-		if (check_newline(buffer))
+		new_i = check_newline(buffer);
+		if (new_i != -1)
 		{
-			line = malloc(check_newline(buffer) + 1);
-			if (!line)
-			{
-				free(buffer);
-				return (0);
-			}
-
-
+			line_extract(buffer, line, new_i);
+			buffer = leftover(buffer, line, new_i);
+			return (line);
 	}
 }
+
 
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: kyungkim <kyungkim@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/07 22:46:55 by kyungkim          #+#    #+#             */
-/*   Updated: 2025/01/07 23:26:28 by kyungkim         ###   ########.fr       */
+/*   Updated: 2025/01/10 00:43:25 by kyungkim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 int	check_newline(char *buffer)
 {
 	int	i;
-
+	
 	i = 0;
 	while (buffer[i])
 	{
@@ -26,27 +26,48 @@ int	check_newline(char *buffer)
 	return (-1);
 }
 
+void	line_extract(char *buffer, char* line, int i)
+{
+	line = malloc(i + 2);
+	if (!line)
+	{
+		free(buffer);
+		return ;
+	}
+	memcopy(line, buffer, i + 1);
+	line[check_newline(buffer) + 1] = '\0';
+}
+
+char	*leftover(char *buffer, char *line, int i)
+{
+	int		left_i;
+	char	*left;
+
+	left_i = 1;
+	while (buffer[i + left_i])
+		left_i++;
+	left = malloc(left_i);
+	if (!left)
+	{
+		free(buffer);
+		free(line);
+		return (0);
+	}
+	memcopy(left, buffer + i + 1, left_i - 1);
+	left[left_i - 1] = '\0';
+	return (left);
+}
+
+
 void	memcopy(void *dest, void *src, int i)
 {
 	int c;
 
 	c = 0;
-int	check_newline(char *buffer)
-{
-	int	i;
-
-	i = 0;
-	while (buffer[i])
-	{
-		if (buffer[i] == '\n')
-			return (i);
-		i++;
-	}
-	return (0);
-}
-	while (c <= i)
+	while (c < i)
 	{
 		((char *)dest)[c] = ((char *)src)[c];
 		c++;
 	}
 }
+
